ad28c5b515b0c162b0d4a6bd0c7b863caac06b6c
javahan13216100272 <401162727@qq.com>
2017-07-30 03:15:08 +0800

    Update getJSON.js

--- ztjt/js/getJSON.js
+++ ztjt/js/getJSON.js
@@ -1,10 +1,39 @@
-function aa(){
+
+window.onload=function(){
+    //getJson();
+    loadJson("https://rawgit.com/javahan13216100272/project4ZT/master/ztjt/itemList.json");
+}
+function getJson(){
+    console.log('getJson')
     $.ajax({
-      url:'https://rawgit.com/javahan13216100272/project4ZT/master/ztjt/listData.json',
+      url:'https://rawgit.com/javahan13216100272/project4ZT/master/ztjt/itemList.json',
       data:'',
       type:'POST',
       dataType:'json',
-      async:false
+      async:false,
+      success:()
     })
 }
-aa();
+function loadJson(url)
+{
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+	{
+		//  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+		xmlhttp=new XMLHttpRequest();
+	}
+	else
+	{
+		// IE6, IE5 浏览器执行代码
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function()
+	{
+		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+		{
+			document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
+		}
+	}
+	xmlhttp.open("GET",url,true);
+	xmlhttp.send();
+}
