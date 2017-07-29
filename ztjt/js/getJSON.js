eba051f93a17afcb3d7c95c7f20995e7e7311902
javahan13216100272 <401162727@qq.com>
2017-07-30 03:09:22 +0800

    Create getJSON.js

---
+++ ztjt/js/getJSON.js
@@ -0,0 +1,10 @@
+function aa(){
+    $.ajax({
+      url:'https://rawgit.com/javahan13216100272/project4ZT/master/ztjt/listData.json',
+      data:'',
+      type:'POST',
+      dataType:'json',
+      async:false
+    })
+}
+aa();
